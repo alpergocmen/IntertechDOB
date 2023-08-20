3bb67b192362cc31494bd17a5f3e4223684e8795
Alper <levindiee@hotmail.com>
2023-08-20 22:23:54 +0300

    two api files added but not working for now

---
+++ api-arka.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from main import KimlikTespit
+
+app = Flask(__name__)
+
+@app.route('/process_back', methods=['POST'])
+def process_photo():
+    try:
+        data = request.json
+        source_path = data.get('source_path', None)
+        print("Received source path:", source_path)  # Add this line
+
+        
+        if source_path is None:
+            return jsonify({'error': 'Image source path is missing.'}), 400
+
+        # Perform image processing using your existing code
+        weights = "weights/arkayuz_weight.pt"
+        kimlikTespit = KimlikTespit(weights, source_path)
+        arkayuz_sonuc = kimlikTespit.kimlik_kontrol()
+        
+        response = {
+            'arkayuz_sonuc': arkayuz_sonuc,
+            # Add other results here
+        }
+        
+        return jsonify(response), 200
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
