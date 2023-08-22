da9813d9c520b8f7bf19613fb550fd185bd227b0
Alper <levindiee@hotmail.com>
2023-08-22 12:32:21 +0300

    api on arka updated, and api_face added

---
+++ api_arka.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+from main import KimlikTespit, img_save
+import os
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/process_back', methods=['POST'])
+def process_photo():
+    try:
+        data = request.json
+        arka_yuz_image = data.get('arka_yuz_image', None)
+        
+        if arka_yuz_image is None:
+            return jsonify({'error': 'Arka yuz data is missing.'}), 400
+        
+        # Perform image processing using your existing code
+        weights = "weights/arkayuz_weight.pt"
+        source = img_save(arka_yuz_image)
+        kimlik_tespit = KimlikTespit(weights, source)
+        arka_yuz_sonuc = kimlik_tespit.kimlik_kontrol()    
+        os.remove(source)
+        
+        response = {
+            'arka_yuz_sonuc': arka_yuz_sonuc,
+            # Add other results here
+        }
+        print(response)
+        
+        df2 = kimlik_tespit.df
+        df1 = pd.read_csv("df.csv")
+        
+        df_concat = pd.concat([df1, df2], ignore_index=False,)
+        df_concat.drop(df_concat.columns[df_concat.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
+        df_concat.reset_index(drop=True, inplace=True)
+        df_concat.to_csv("df_concat.csv", index=False)
+        
+        os.remove("df.csv")
+        return jsonify(response), 200
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
--- api_on.py
+++ api_on.py
@@ -1,37 +1,42 @@
 from flask import Flask, request, jsonify
 from main import KimlikTespit, img_save
-import base64
-import numpy as np
 import os
+import pandas as pd 
+
 
 app = Flask(__name__)
 
-@app.route('/process_back', methods=['POST'])
+@app.route('/process_front', methods=['POST'])
 def process_photo():
     try:
         data = request.json
-        base64_image = data.get('base64_image', None)
+        on_yuz_image = data.get('on_yuz_image', None)
         
-        if base64_image is None:
-            return jsonify({'error': 'Base64 image data is missing.'}), 400
-        
-        print(base64_image)
+        if on_yuz_image is None:
+            return jsonify({'error': 'On yuz data is missing.'}), 400
+      
         # Perform image processing using your existing code
-        weights = "weights/arkayuz_weight.pt"
-        source = img_save(base64_image)
-        kimlikTespit = KimlikTespit(weights, source)
-        arkayuz_sonuc = kimlikTespit.kimlik_kontrol()
+        weights = "weights/onyuz_weight.pt"
+        source = img_save(on_yuz_image)
+        kimlik_tespit = KimlikTespit(weights, source)
+        on_yuz_sonuc = kimlik_tespit.kimlik_kontrol()
         os.remove(source)
         
+        df = kimlik_tespit.df
+        df.to_csv("df.csv")
+        
         response = {
-            'arkayuz_sonuc': arkayuz_sonuc,
+            'on_yuz_sonuc': on_yuz_sonuc,
             # Add other results here
         }
         print(response)
+        
         return jsonify(response), 200
     
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
